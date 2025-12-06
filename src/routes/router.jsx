ed8c7760906ddb62902e907deae79fc1aedd4902
HasinIshrakK <hasinishrakk@gmail.com>
2025-12-07 02:11:03 +0600

    Error page added

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,15 +1,24 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Link } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
 import AllScholarships from "../pages/AllScholarships/AllScholarships";
 import Login from "../pages/Auth/Login/Login";
 import Register from "../pages/Auth/Register/Register";
 import Auth from "../pages/Auth/Auth";
+import Error from "../components/Error/Error";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         Component: RootLayout,
+        errorElement: <div className="min-h-screen items-center flex flex-col">
+            <Error></Error>
+            <Link to='/'>
+                <button className="btn btn-primary">
+                    Go Home
+                </button>
+            </Link>
+        </div>,
         children: [
             {
                 index: true,
@@ -27,6 +36,17 @@ export const router = createBrowserRouter([
                     { path: "login", Component: Login },
                     { path: "register", Component: Register },
                 ],
+            },
+            {
+                path: "/*",
+                element: <div className="min-h-screen items-center flex flex-col">
+                    <Error></Error>
+                    <Link to='/'>
+                        <button className="btn btn-primary">
+                            Go Home
+                        </button>
+                    </Link>
+                </div>,
             },
         ],
     },
